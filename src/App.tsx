7a298296092401efcd24a5f0b4682bb65e7f16f8
Gavin Garza <gaving@live.com>
2023-11-01 19:19:57 +0900

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Horse, Heart, Cube } from "@phosphor-icons/react"
+
+const App = () => {
+  return (
+    <main>
+      <Horse />
+      <Heart color="#AE2983" weight="fill" size={32} />
+      <Cube color="teal" weight="duotone" />
+    </main>
+  )
+}
+
+
+export default App
