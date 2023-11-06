4e10f70bb4792fde213dc568bad952529494b819
Gavin Garza <gaving@live.com>
2023-11-07 08:39:10 +0900

    tailwind config refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundColor: {
+      colors: {
         "Green/500": "#38C97C",
         "Grey/200": "#A7AEB4",
         "Grey/400": "#8C959D",
@@ -17,21 +17,11 @@ export default {
         "Primary/200": "#B3D6F3",
         "Primary/500": "#4299E1",
         "White/50": "#FFFFFF80",
-      },
-      textColor: {
         "Text/Main": "#2E3F51",
         "Text/Menu": "#5F799C",
-        "Grey/400": "#8C959D",
-        "Grey/800": "#596269",
-        "Info/600": "#1E54B7",
       },
       fontSize: {
         32: "32px",
-      },
-      colors: {
-        "Neutral/500": "#DEE1E3",
-        "Primary/100": "#D9EBF9",
-        "Primary/500": "#4299E1",
       },
       padding: {
         main: "60px",
